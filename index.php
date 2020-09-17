5f45c83002ec2e9e86f6176635450618593d911f
dzakyfaturrahman <68413234+dzakyfaturrahman@users.noreply.github.com>
2020-09-17 12:33:33 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+echo '<html>';
+echo '<header>';
+echo '<title>Latihan 1</title>';
+echo '</header>';
+
+echo "<body align=\"justify\" style=\"font-family:calibri; color:white; background-image: url('https://64.media.tumblr.com/5809b19c304d62e818b0a1c5ab5da410/tumblr_inline_mlkyba3ZkV1r53miq.png');\">\n";
+
+echo '';
+echo '<p>Nama: Dzaky Fatur Rahman</p>';
+echo '<p>Kelas: XII-RPL</p>';
+echo '';
+echo '<hr>';
+echo '';
+echo '<h2 align="center">R.A Kartini</h2>';
+echo '';
+
+echo "<div align=\"center\"><img height=\"40%\" align=\"center\" src=\"https://upload.wikimedia.org/wikipedia/commons/2/23/COLLECTIE_TROPENMUSEUM_Portret_van_Raden_Ajeng_Kartini_TMnr_10018776.jpg\"></div>\n";
+
+echo '';
+echo '<div style="padding-left:10%; padding-right:10%;">';
+echo '';
+echo '<p><u>Orientasi</u></p>';
+echo '<p>&nbsp;&nbsp;&nbsp;&nbsp;<i>Raden Ajeng Kartini</i> atau yang kita kenal dengan Ibu Kartini. Dia adalah salah satu keturunan keluarga terpandang yang lahir pada tanggal <b>21 April 1879</b>, dan keluarganya yang mewariskan suatu hal yaitu pendidikan. Beliau pernah duduk dibangku sekolah dasar sampai tamat sekolah sekolah dasar. Beliau tidak pernah puas akan ilmu pengetahuan dan membuat beliau ingin melanjutkan ke jenjang yang lebih tinggi. Namun ayahnya tidak sependapat dengan beliau untuk melanjutkan pendidikanya. Tahu sikap ayahnya sperti itu beliau sedih namun tidak bisa mengubah keputusan ayahnya</p>';
+echo '';
+echo '<p><u>Peristiwa dan Masalah</u></p>';
+echo '<p>&nbsp;&nbsp;&nbsp;&nbsp;Adat dikeluarganya yaitu seorang gadis atau wanita yang belum menikah belum dibolehkan keluar rumah atau juga disebut dipingit. Untuk mengisi waktu luangnya beliau membaca buku ilmu pengetahuan yang ia miliki. Beliau memang gemar membaca atau kutu buku dan menjadi keseharianya saat banyak waktu luang. Bahkan dia tidak takut untuk bertanya kepada ayahnya bila dia tidak mengerti atau kurang paham. Kartini mempunyai teman yang banyak di Belanda dan sering bekomunikasi dengan mereka. Bahkan pernah meminta kepada <b>Mr.J.H. Abendanon</b> untuk memberikan dirinya beasiswa untuk bersekolah di Belanda. Belum sempat menyampaikan keinginanya beliau dinikahkan dengan Adipati Rembang yang bernama Raden Adipati Oyodiningrat. Walaupun begitu beliau tidak berhenti untuk bercita cita dan karena suaminya pula mendukung cita citanya. Dengan ketekunan dan kegigihan dari beliau dan suaminya mendirikan sekolahan wanita di Semarang, Surabaya, Yogyakarta, Madiun, Cirebon, dan Malang. Sekolahan tersebut diberi nama dengan dengan sekolahan kartini. Pada tanggal 17 September 1904 Kartini meninggal dunia pada usia 25 saat melahirkan anak pertama dan satu-satunya. Kemudian kisah beliau menjadi pelopor emansipasi wanita ditanah jawa. Kemudian kisah R.A Kartini di bukukan oleh Abendanon dengan judul “Door Duistemis Tot Licht” atau yang kita kenal dengan “Habis Gelap Terbitlah Terang”. Buku ini telah menginspirasi wanita di Indonesia tidak hanya pada waktu itu tapi sampai sekarang.</p>';
+echo '';
+echo '<p><u>Reorientasi</u></p>';
+echo '<p>&nbsp;&nbsp;&nbsp;&nbsp;Kita tidak boleh melupakan jasa R.A Kartini  tetapi, kita harus mengenang jasa dan meniru sifatnya yang pantang menyerah terhadap masalah apapun. Karena setia masalah pasti ada jalan keluarnya.</p>';
+echo '';
+echo '<p>Sumber:</p>';
+echo '<p><a style="color:white;" href="https://www.yuksinau.id/contoh-teks-cerita-sejarah/">https://www.yuksinau.id/contoh-teks-cerita-sejarah/</a></p>';
+echo '';
+echo '</div>';
+echo '';
+echo '</body>';
+echo '</html>';
+
+?>
